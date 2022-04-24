65975b0c5b1de316673abc9de5c2c8fb2a889cc9
Kamil Bauer <259102@student.pwr.edu.pl>
2022-04-24 15:07:28 +0200

    Added menu

--- main.py
+++ main.py
@@ -4,40 +4,43 @@ from Sender import Sender
 from Receiver import Receiver
 from Chanel import Chanel
 from Generator import Generator
+from menu import Menu
 
-m = 42                       #dlugosc ciagu bitow
-n = 8                         #dlugosc pakietu
-p = 0.0                          #prawdopodobieństwo
-
-generator = Generator(m)            #generowanie ciągu 0 i 1
-data = generator.generate_data()
-
-# print(' 1.Bit parzystosci\n 2.Suma kontrolna\n 3.Bit parzystosci i suma kontrolna\n')
-# choice = int(input("Wprowadz swoj wybor: "))
-# print(' 1.BSC\n 2.BEC\n 3.BSC i BEC\n')
-# choice2 = int(input("Wprowadz swoj wybor: "))
-
-sender = Sender(n, m, data)             #kodowanie
-packets = sender.coder_parity_bit()
-# packets = sender.coder_check_sum(m, data)
-# packets = sender.coder_parity_bit_and_check_sum()
-
-print("Wyslany:")
-print(packets)
-
-chanel = Chanel(packets, p)             #zaklocenia
-for i in range(0, numpy.shape(chanel.packets)[0]):
-    packets[i] = chanel.BSC(chanel.p, chanel.packets[i])
-    # packets[i] = chanel.BEC(chanel.p, chanel.packets[i])
-
-print("Otrzymany:")
-print(packets)
-
-receiver = Receiver(packets, n)         #dekodowanie
-answer = receiver.decoder_parity_bit()
-# answer = receiver.decoder_check_sum()
-# answer = receiver.decoder_parity_bit_and_check_sum()
+menu = Menu(42,8,0.1,100)
+# m = 42                       #dlugosc ciagu bitow
+# n = 8                         #dlugosc pakietu
+# p = 0.5                          #prawdopodobieństwo
 
 
-if answer: print("Odpowiedz: Zaakceptowano")
-else: print("Odpowiedz: Odrzucono")
+# generator = Generator(m)            #generowanie ciągu 0 i 1
+# data = generator.generate_data()
+
+# # print(' 1.Bit parzystosci\n 2.Suma kontrolna\n 3.Bit parzystosci i suma kontrolna\n')
+# # choice = int(input("Wprowadz swoj wybor: "))
+# # print(' 1.BSC\n 2.BEC\n 3.BSC i BEC\n')
+# # choice2 = int(input("Wprowadz swoj wybor: "))
+
+# sender = Sender(n, m, data)             #kodowanie
+# packets = sender.coder_parity_bit()
+# # packets = sender.coder_check_sum(m, data)
+# # packets = sender.coder_parity_bit_and_check_sum()
+
+# print("Wyslany:")
+# print(packets)
+
+# chanel = Chanel(packets, p)             #zaklocenia
+# for i in range(0, numpy.shape(chanel.packets)[0]):
+#     packets[i] = chanel.BSC(chanel.p, chanel.packets[i])
+#     # packets[i] = chanel.BEC(chanel.p, chanel.packets[i])
+
+# print("Otrzymany:")
+# print(packets)
+
+# receiver = Receiver(packets, n)         #dekodowanie
+# answer = receiver.decoder_parity_bit()
+# # answer = receiver.decoder_check_sum()
+# # answer = receiver.decoder_parity_bit_and_check_sum()
+
+
+# if answer: print("Odpowiedz: Zaakceptowano")
+# else: print("Odpowiedz: Odrzucono")
---
+++ menu.py
@@ -0,0 +1,93 @@
+from base64 import encode
+import komm
+import numpy
+from Sender import Sender
+from Receiver import Receiver
+from Chanel import Chanel
+from Generator import Generator
+
+class Menu:
+
+
+    def __init__(self, msgLen, packetLen, probability, numOfIter):
+        self.msgLen = msgLen                       #bit message length
+        self.packetLen = packetLen                        #packet length
+        self.probability = probability                      #probability
+        self.numOfIter = numOfIter                     #number of iterations
+        self.data = [0]
+        self.packets = [0]
+        
+        
+        print(' 1.Bit parzystosci\n 2.Suma kontrolna\n 3.Bit parzystosci i suma kontrolna\n')
+        self.choice = int(input("Wprowadz swoj wybor: "))
+
+        print(' 1.BSC\n 2.BEC\n 3.BSC i BEC\n')
+        self.choice2 = int(input("Wprowadz swoj wybor: "))
+
+        self.generator = Generator(self.msgLen)
+       
+
+        self.sender = Sender(self.packetLen, self.msgLen, self.data) 
+        self.chanel = Chanel(self.packets, self.probability)
+        self.receiver = Receiver(self.packets, self.packetLen)
+
+    def _sendNew_(self):
+        self.sender.data = self.generator.generate_data()             #generowanie ciągu 0 i 1
+
+    def _send_(self):
+        self.packets = self._encode_()
+
+        print("Wyslany:")
+        print(self.packets)
+
+        self.chanel.packets = self.packets
+        self._addNoise_()
+
+        print("Otrzymany:")
+        print(self.packets)
+
+        self.receiver.packets=self.packets
+        answear = self._decode_()
+        if answear: print("Odpowiedz: Zaakceptowano")
+        else: 
+            print("Odpowiedz: Odrzucono")
+            self._send_()
+
+
+
+
+    def _encode_(self):
+        if self.choice==1:
+            return self.sender.coder_parity_bit()
+        if self.choice==2:
+            return self.sender.coder_check_sum(self.msgLen, self.data)
+        if self.choice==3:
+            return self.sender.coder_parity_bit_and_check_sum()
+
+
+    def _addNoise_(self):       #zaklocenia
+        if self.choice2==1:
+            for i in range(0, numpy.shape(self.chanel.packets)[0]):
+                self.packets[i] = self.chanel.BSC(self.chanel.p, self.chanel.packets[i])
+
+        if self.choice2==2:
+            for i in range(0, numpy.shape(self.chanel.packets)[0]):
+                self.packets[i] = self.chanel.BEC(self.chanel.p, self.chanel.packets[i])
+
+        else:
+            for i in range(0, numpy.shape(self.chanel.packets)[0]):
+                self.packets[i] = self.chanel.BSC(self.chanel.p, self.chanel.packets[i])
+            for i in range(0, numpy.shape(self.chanel.packets)[0]):
+                self.packets[i] = self.chanel.BEC(self.chanel.p, self.chanel.packets[i])
+
+
+
+    def _decode_(self):
+        if self.choice==1:
+            return self.receiver.decoder_parity_bit()
+        if self.choice==2:
+            return self.receiver.decoder_check_sum()
+        if self.choice==3:
+            return self.receiver.decoder_parity_bit_and_check_sum()
+        #dekodowanie
+
